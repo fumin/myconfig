1185c10e5d33fb2409ab92cfdc6c372b001c57d3
shaoyu <awawfumin@gmail.com>
2025-12-18 18:24:48 +0800

    add lxc config

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ fi
 
 ln -s $PWD/bashrc $HOME/.mybash
 ln -s $PWD/vimrc $HOME/.vimrc
+ln -s $PWD/lxc.conf $HOME/.config/lxc/default.conf
 
 mkdir -p $FIREFOX_PROFILE/chrome/distribution
 ln -s $PWD/firefox/userChrome.css $FIREFOX_PROFILE/chrome/userChrome.css
