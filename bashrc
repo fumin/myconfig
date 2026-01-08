0f46a5916058473275d31c0c061ee1a35644041f
shaoyu <awawfumin@gmail.com>
2026-01-08 13:44:31 +0800

    disable firefox top right window dragging

--- bashrc
+++ bashrc
@@ -8,7 +8,14 @@ alias pbcopy='xclip -selection clipboard'
 alias pbpaste='xclip -selection clipboard -o'
 
 open() {
-        nautilus $1 &
+        FINDER="gio open"
+        if type explorer.exe > /dev/null 2>&1; then
+                FINDER=explorer.exe
+        fi
+
+        # Remove process start/stop notifications by running command in a subshell.
+        # This is done by wrapping command with parenthesis.
+        ($FINDER $1 &)
 }
 
 # Fix no sound.
@@ -16,6 +23,12 @@ open() {
 # /etc/modprobe.d/alsa-base.conf
 # options snd-hda-intel dmic_detect=0
 
+# yt-dlp for downloading Youtube.
 ayt-dlp () {
 	yt-dlp -f "bv[height<=720]+ba/b[height<=720]" -o "%(id)s.%(ext)s" $1
+}
+
+# Shortcut to activate python's virtual environment.
+avp () {
+	source .venv/bin/activate
 }
--- setup.sh
+++ setup.sh
@@ -6,11 +6,27 @@ fi
 
 ln -s $PWD/bashrc $HOME/.mybash
 ln -s $PWD/vimrc $HOME/.vimrc
-ln -s $PWD/lxc.conf $HOME/.config/lxc/default.conf
+mkdir -p $HOME/.config/autostart
 for FNAME in `ls $PWD/autostart`; do
 	ln -s $PWD/autostart/$FNAME $HOME/.config/autostart/$FNAME
 done
 
+# Lxc.
+# Configure /etc/lxc/lxc-usernet.
+LXC_NET_PATH=/etc/lxc/lxc-usernet
+LXC_NET_CONF="$(id -un) veth lxcbr0 10"
+if ! grep -q -F -x "$LXC_NET_CONF" $LXC_NET_PATH; then
+	echo "$(id -un) veth lxcbr0 10" | sudo tee -a $LXC_NET_PATH > /dev/null
+fi
+# lxc maps the rootfs under $HOME/.local, so setup permissions for it.
+chmod +x $HOME
+chmod +x $HOME/.local
+chmod +x $HOME/.local/share
+# Setup our local config.
+mkdir -p $HOME/.config/lxc
+ln -s $PWD/lxc.conf $HOME/.config/lxc/default.conf
+
+# Firefox.
 mkdir -p $FIREFOX_PROFILE/chrome/distribution
 ln -s $PWD/firefox/userChrome.css $FIREFOX_PROFILE/chrome/userChrome.css
 ln -s $PWD/firefox/userContent.css $FIREFOX_PROFILE/chrome/userContent.css
