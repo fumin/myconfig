728a3e264d3ad5634c26461b9f5eeaf72fc19ed8
shaoyu <awawfumin@gmail.com>
2026-01-08 13:44:31 +0800

    disable firefox top right window dragging

--- setup.sh
+++ setup.sh
@@ -6,11 +6,20 @@ fi
 
 ln -s $PWD/bashrc $HOME/.mybash
 ln -s $PWD/vimrc $HOME/.vimrc
-ln -s $PWD/lxc.conf $HOME/.config/lxc/default.conf
+mkdir -p $HOME/.config/autostart
 for FNAME in `ls $PWD/autostart`; do
 	ln -s $PWD/autostart/$FNAME $HOME/.config/autostart/$FNAME
 done
 
+# Lxc.
+mkdir -p $HOME/.config/lxc
+ln -s $PWD/lxc.conf $HOME/.config/lxc/default.conf
+# lxc maps the rootfs under $HOME/.local, so setup permissions for it.
+chmod +x $HOME
+chmod +x $HOME/.local
+chmod +x $HOME/.local/share
+
+# Firefox.
 mkdir -p $FIREFOX_PROFILE/chrome/distribution
 ln -s $PWD/firefox/userChrome.css $FIREFOX_PROFILE/chrome/userChrome.css
 ln -s $PWD/firefox/userContent.css $FIREFOX_PROFILE/chrome/userContent.css
